39d3d68bef4abe530381ebe5da3f3b87c0e0b2ec
hoshmandaqil <aqil1995.hoshmand@gmail.com>
2023-09-21 21:20:49 +0430

    Add more data to Kabul province seeder

--- database/seeders/KabulProvinceSeeder.php
+++ database/seeders/KabulProvinceSeeder.php
@@ -11,27 +11,72 @@ class KabulProvinceSeeder extends Seeder
     {
         // Create province
         $province = Province::create([
-            'name' => 'کابل',
-            'en_name' => 'Kabul',
+            "name_en"=> "Kabul",
+            "name_dr"=> "کابل",
         ]);
 
         // Create districts
         $province->districts()->createMany([
-            ['name' => 'بگرامی', 'en_name' => 'Bagrami'],
-            ['name' => 'چهار آسیاب', 'en_name' => 'Chahar Asyab'],
-            ['name' => 'ده سبز', 'en_name' => 'Deh Sabz'],
-            ['name' => 'فرزه', 'en_name' => 'Farza'],
-            ['name' => 'گل دره', 'en_name' => 'Guldara'],
-            ['name' => 'استالف', 'en_name' => 'Istalif'],
-            ['name' => 'شهر کابل', 'en_name' => 'Kabul (city)'],
-            ['name' => 'کلکان', 'en_name' => 'Kalakan'],
-            ['name' => 'خاک جبار', 'en_name' => 'Khaki Jabbar'],
-            ['name' => 'میر بچه کوت', 'en_name' => 'Mir Bacha Kot'],
-            ['name' => 'موسهیی', 'en_name' => 'Mussahi'],
-            ['name' => 'پغمان', 'en_name' => 'Paghman'],
-            ['name' => 'قره باغ', 'en_name' => 'Qarabagh'],
-            ['name' => 'شکردره', 'en_name' => 'Shakardara'],
-            ['name' => 'سروبی', 'en_name' => 'Surobi'],
+            [
+              "name_en"=>"Provincial Capital of Kabul (Kabul)",
+              "name_dr"=>"شهر کابل",
+            ],
+            [
+              "name_en"=>"BAGRAMI",
+              "name_dr"=>"بگرامی",
+            ],
+            [
+              "name_en"=>"DEHSABZ",
+              "name_dr"=>"ده سبز",
+            ],
+            [
+              "name_en"=>"SHAKAR DARA",
+              "name_dr"=>"شکردره",
+            ],
+            [
+              "name_en"=>"MUSAHI",
+              "name_dr"=>"موسئی",
+            ],
+            [
+              "name_en"=>"MIR BACHA KOT",
+              "name_dr"=>"میر بچه کوت",
+            ],
+            [
+              "name_en"=>"KHAK-E-JABAR",
+              "name_dr"=>"خاک جبار",
+            ],
+            [
+              "name_en"=>"KALAKAN",
+              "name_dr"=>"کلکان",
+            ],
+            [
+              "name_en"=>"GULDARA",
+              "name_dr"=>"گلدره",
+            ],
+            [
+              "name_en"=>"FARZA",
+              "name_dr"=>"فرزه",
+            ],
+            [
+              "name_en"=>"ESTALEF",
+              "name_dr"=>"استالف",
+            ],
+            [
+              "name_en"=>"QARA BAGH",
+              "name_dr"=>"قره باغ",
+            ],
+            [
+              "name_en"=>"SURUBI",
+              "name_dr"=>"سروبی",
+            ],
+            [
+              "name_en"=>"Paghman",
+              "name_dr"=>"پغمان",
+            ],
+            [
+              "name_en"=>"CHAHAR ASYAB",
+              "name_dr"=>"چهارآسیاب",
+            ],
         ]);
     }
 }
