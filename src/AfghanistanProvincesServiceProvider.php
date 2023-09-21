e51ce46596f63e8393c57a892f6240c70d75edb1
hamidafghan <abd.hamidaf@gmail.com>
2023-09-21 12:54:06 +0200

    Add the tag

--- src/AfghanistanProvincesServiceProvider.php
+++ src/AfghanistanProvincesServiceProvider.php
@@ -16,6 +16,6 @@ class AfghanistanProvincesServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__ . '/../database/migrations/create_provinces_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_provinces_table.php'),
-        ], 'migrations');
+        ], 'afghanistan-provinces-migrations');
     }
 }
