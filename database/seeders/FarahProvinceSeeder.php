7e068dc58ec5191b30ab177d0466e8470aafd322
hoshmandaqil <aqil1995.hoshmand@gmail.com>
2023-10-02 21:32:47 +0430

    add farah district

--- database/seeders/FarahProvinceSeeder.php
+++ database/seeders/FarahProvinceSeeder.php
@@ -17,8 +17,16 @@ class FarahProvinceSeeder extends Seeder
 
         // Create districts
         $province->districts()->createMany([
-            // Add districts here...
-            // ['name' => 'نام', 'en_name' => 'Name'],
+            ["en_name"=>"PROVINCIAL CAPITAL OF  FARAH ( FARAH )","name"=>"مرکز فراه"],            ["en_name"=>"PUSHTRUD","name"=>"پشت رود"],
+            ["en_name"=>"KHAK-E-SAFED","name"=>"خاک سفید"],
+            ["en_name"=>"QALA-E-KAH","name"=>"قلعه کاه"],
+            ["en_name"=>"SHIB KOH","name"=>"شیب کوه"],
+            ["en_name"=>"BALA BULUK","name"=>"بالا بلوک"],
+            ["en_name"=>"ANAR DARA","name"=>"اناردره"],
+            ["en_name"=>"BAKWA","name"=>"بکواه"],
+            ["en_name"=>"LASH-E-JUWAYN", "name"=>"لاش جوین"],
+            ["en_name"=>"GULISTAN","name"=>"گلستان"],
+            ["en_name"=>"PUR CHAMAN","name"=>"پرچمن"],
         ]);
     }
 }
