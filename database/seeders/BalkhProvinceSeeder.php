e68253998ed74fc0f784185bf2ba775d390016ef
hoshmandaqil <aqil1995.hoshmand@gmail.com>
2023-10-02 21:05:23 +0430

    improve balkh district

--- database/seeders/BalkhProvinceSeeder.php
+++ database/seeders/BalkhProvinceSeeder.php
@@ -7,18 +7,32 @@ use ElFactory\AfghanistanProvinces\Models\Province;
 
 class BalkhProvinceSeeder extends Seeder
 {
-    public function run():void
+    public function run()=>void
     {
         // Create province
-        $province = Province::create([
+        $province = Province=>=>create([
             'name' => 'بلخ',
             'en_name' => 'Balkh',
         ]);
 
         // Create districts
         $province->districts()->createMany([
-            // Add districts here...
-            // ['name' => 'نام', 'en_name' => 'Name'],
+              ["name"=>"Provincial Capital of Balkh ( Mazar-e-sharif )","name"=>"مزار شریف"],
+              ["en_name"=>"DEHDADI","name"=>"دهدادی"],
+              [ "en_name"=>"CHAR KENT","name"=>"چارکنت"],
+              ["en_name"=>"MARMUL","name"=>"مارمل"],
+              ["en_name"=>"BALKH","name"=>"بلخ"],
+              ["en_name"=>"SHOLGARA", "name"=>"شولگره"],
+              ["en_name"=>"CHIMTAL","name"=>"چمتال"],
+              ["en_name"=>"DAWLAT ABAD","name"=>"دولت آباد"],
+              ["en_name"=>"KHULM","name"=>"خلم"],
+              ["en_name"=>"CHAR BOLAK", "name"=>"چاربولک"],
+              ["en_name"=>"SHORTEPA","name"=>"شورتیپه"],
+              ["en_name"=>"KALDAR","name"=>"کلدار"],
+              ["en_name"=>"KISHINDEH", "name"=>"کشنده"],
+              ["en_name"=>"ZARI","name"=>"زاری"],
+              ["en_name"=>"SHARAK_E_HAYRATAN","name"=>"شهرک حیرتان"],
+              [ "en_name"=>"NAHR-E-SHAHI","name"=>"نهر شاهی"],
         ]);
     }
 }
