5c7d9109812a1dacba562961764fb1a70e9aebcf
hamidafghan <abd.hamidaf@gmail.com>
2023-10-29 22:59:54 +0100

    Add Kandahar districts

--- database/seeders/KandaharProvinceSeeder.php
+++ database/seeders/KandaharProvinceSeeder.php
@@ -17,8 +17,23 @@ class KandaharProvinceSeeder extends Seeder
 
         // Create districts
         $province->districts()->createMany([
-            // Add districts here...
-            // ['name' => 'نام', 'en_name' => 'Name'],
+            ['name' => 'کندهار', 'en_name' => 'Kandahar City', 'is_center' => true],
+            ['name' => 'ارغنداب', 'en_name' => 'Arghandab'],
+            ['name' => 'ارغستان', 'en_name' => 'Arghistan'],
+            ['name' => 'دامان', 'en_name' => 'Daman'],
+            ['name' => 'غورک', 'en_name' => 'Ghorak'],
+            ['name' => 'خاکریز', 'en_name' => 'Khakrez'],
+            ['name' => 'معروف', 'en_name' => 'Maruf'],
+            ['name' => 'میوند', 'en_name' => 'Maywand'],
+            ['name' => 'میانشین', 'en_name' => 'Miyan Nasheen'],
+            ['name' => 'نیش', 'en_name' => 'Naish'],
+            ['name' => 'پنجوایی', 'en_name' => 'Panjwaye'],
+            ['name' => 'ریگ', 'en_name' => 'Reg'],
+            ['name' => 'شاه ولی کوت', 'en_name' => 'Shah Wali Kot'],
+            ['name' => 'شورابک', 'en_name' => 'Shorabak'],
+            ['name' => 'سپین بولدک', 'en_name' => 'Spin Boldak'],
+            ['name' => 'ژیری', 'en_name' => 'Zharai'],
+            ['name' => 'دند', 'en_name' => 'Dand'],
         ]);
     }
 }
