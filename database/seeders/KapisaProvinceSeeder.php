2cce7ec0bed7e90fd4bd93a172e3e26783e6dbaa
hamidafghan <abd.hamidaf@gmail.com>
2023-11-01 16:07:10 +0100

    Add Kapisa province districts

--- database/seeders/KapisaProvinceSeeder.php
+++ database/seeders/KapisaProvinceSeeder.php
@@ -17,8 +17,22 @@ class KapisaProvinceSeeder extends Seeder
 
         // Create districts
         $province->districts()->createMany([
-            // Add districts here...
-            // ['name' => 'نام', 'en_name' => 'Name'],
+            ['name' => 'محمود راقی', 'en_name' => 'Mahmud Raqi', 'is_center' => true],
+            ['name' => 'آلاسای', 'en_name' => 'Alasay'],
+            ['name' => 'حصه اول کوهستان', 'en_name' => 'Hesa Awal Kohistan'],
+            ['name' => 'حصه دوم کوهستان', 'en_name' => 'Hesa Duwum Kohistan'],
+            ['name' => 'کوه بند', 'en_name' => 'Koh Band'],
+            ['name' => 'نجراب', 'en_name' => 'Nijrab'],
+            ['name' => 'تگاب', 'en_name' => 'Tagab'],
+            ['name' => 'کوهستان', 'en_name' => 'Kuhistan'],
+            ['name' => 'موجود نیست', 'en_name' => 'N/A'],
+            ['name' => 'مرکز کاپیسا', 'en_name' => 'Center of Kapisa'],
+            ['name' => 'کوهستان-1', 'en_name' => 'Kohistan-1'],
+            ['name' => 'کوهستان-2', 'en_name' => 'Kohistan-2'],
+            ['name' => 'نجراب', 'en_name' => 'Nijrab.'],
+            ['name' => '', 'en_name' => 'Hasa Awal Kohistan'],
+            ['name' => 'حصه اول', 'en_name' => 'Hesa Awal'],
+            ['name' => 'حصه دوم', 'en_name' => 'Hesa Dowm'],
         ]);
     }
 }
